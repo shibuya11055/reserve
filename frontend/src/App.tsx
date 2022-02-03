06518efa550973e910f3056b2900be6ab1bb47ac
kyohei.shibuya <shibuya01055@gmail.com>
2022-02-04 01:35:22 +0900

    絶対パスの設定

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
-import TestComponent from './test_graphql/testComponent'
-import client from './graphql/client'
+// import TestComponent from './test_graphql/testComponent'
+import TestComponent from '@/test_graphql/testComponent'
+import client from '@/graphql/client'
 import { ApolloProvider } from '@apollo/client'
 
 function App() {
