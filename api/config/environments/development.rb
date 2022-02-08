7120e87d0e0f0ab7933ede04439ca3763c1be639
kyohei.shibuya <shibuya01055@gmail.com>
2022-02-09 01:52:35 +0900

    deviseのセットアップ

--- api/config/environments/development.rb
+++ api/config/environments/development.rb
@@ -61,6 +61,19 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => ENV['GOOGLE_MAIL_ADDRESS'], #gmailアドレス
+    :password => ENV['GOOGLE_PASSWORD'], #gmailパスワード
+    :authentication => 'login',
+  }
+  config.action_mailer.delivery_method = :letter_opener_web
+
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
   post "/graphql", to: "graphql#execute"
+
+  mount_devise_token_auth_for 'User', at: 'api/v1/auth', controllers: {
+    registrations: 'api/v1/auth/registrations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
