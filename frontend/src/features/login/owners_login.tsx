c699eda0baed65b4515616f537166ff510d276cc
kyohei.shibuya <shibuya01055@gmail.com>
2022-03-10 11:12:44 +0900

    ログイン画面の作成

--- frontend/src/features/login/owners_login.tsx
+++ frontend/src/features/login/owners_login.tsx
@@ -1,22 +1,106 @@
+import * as React from 'react'
+import Avatar from '@mui/material/Avatar'
+import Button from '@mui/material/Button'
+import CssBaseline from '@mui/material/CssBaseline'
+import TextField from '@mui/material/TextField'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Checkbox from '@mui/material/Checkbox'
+import Link from '@mui/material/Link'
+import Grid from '@mui/material/Grid'
+import Box from '@mui/material/Box'
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
+import Typography from '@mui/material/Typography'
+import Container from '@mui/material/Container'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 import reserveAxios from '@/lib/reserveAxios'
 
-const onClickLogin = async () => {
-  const res = await reserveAxios.post('/auth/owners/sign_in', {
-    email: 'test1@example.com',
-    password: 'password1234!',
-  })
+const theme = createTheme()
 
-  console.log(res.data)
-}
-function OwnersLogin() {
+export default function OwnersLogin() {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const data = new FormData(event.currentTarget)
+    const res = await reserveAxios.post('/auth/owners/sign_in', {
+      email: data.get('email'),
+      password: data.get('password'),
+    })
+
+    // eslint-disable-next-line no-console
+    console.log(res.data)
+  }
+
   return (
-    <div>
-      <p>ログイン画面</p>
-      <button type="button" onClick={onClickLogin}>
-        ログイン
-      </button>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            管理者ログイン
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="メールアドレス"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="パスワード"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="ログインを保持する"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              ログイン
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                {/* eslint-disable jsx-a11y/anchor-is-valid */}
+                <Link href="#" variant="body2">
+                  パスワードをお忘れの場合
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  新規登録
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   )
 }
-
-export default OwnersLogin
