dca0b0fb9705290b6c20fe7e97646fd0f9681360
kyohei.shibuya <shibuya01055@gmail.com>
2022-03-13 17:27:51 +0900

    eslintのルール調整

--- frontend/src/features/test_components/components/testFieldQueryResponse.ts
+++ frontend/src/features/test_components/components/testFieldQueryResponse.ts
@@ -1,7 +1,6 @@
 import { useTestFieldQuery } from '@/features/test_components/graphql/testField.generated'
 import { useEffect, useState } from 'react'
 
-// eslint-disable-next-line import/prefer-default-export
 export const useTestFieldQueryResponse = () => {
   const { data } = useTestFieldQuery()
   const [testField, setTestField] = useState('')
