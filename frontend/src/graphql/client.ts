97f20097599ef314a15e8ae412a403b375a866da
kyohei.shibuya <shibuya01055@gmail.com>
2022-04-03 00:49:37 +0900

    ログイン時、ヘッダーにアクセストークンを追加する処理を実装

--- frontend/src/graphql/client.ts
+++ frontend/src/graphql/client.ts
@@ -1,11 +1,24 @@
-import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+
 // TODO: 本番環境のドメインが確定したら向け先を追加する必要がある
 const uri =
   process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3000/graphql'
 
+const httpLink = createHttpLink({ uri })
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token')
+  return {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  }
+})
+
 export default new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri,
-  }),
 })
